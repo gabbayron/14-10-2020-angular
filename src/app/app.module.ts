964fc7041d96e5551ae7fb377e6d4e880f4cf25d
gabbayron <gabbayron2307@gmail.com>
2020-10-14 09:31:27 +0300

    added new components and format app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
